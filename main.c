95d707e37e8cf75336da3709d8ec6d122c36f043
BuildTools <unconfigured@null.spigotmc.org>
2020-11-11 09:05:25 +0100

    Initial Commit. Second try.

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * smart_green_smart_house_iot.c
+ *
+ * Created: 11/11/2020 8:22:59 AM
+ * Author : mmate
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	
+	puts("Program started!");
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
